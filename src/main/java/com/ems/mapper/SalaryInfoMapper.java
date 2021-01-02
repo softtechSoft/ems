1551440530e1728f3cba0148cb3206d79cd02e48
softtechSoft <65577291+softtechSoft@users.noreply.github.com>
2021-01-02 13:26:44 +0900

    Add files via upload

---
+++ src/main/java/com/ems/mapper/SalaryInfoMapper.java
@@ -0,0 +1,14 @@
+package com.ems.mapper;
+
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ems.entity.SalaryInfo;
+
+@Mapper
+public interface SalaryInfoMapper
+
+{
+	SalaryInfo querySalaryInfo(Map<String,String> map);
+}
