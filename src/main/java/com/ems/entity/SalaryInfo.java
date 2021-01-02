bb9072dccf7d24421ab0ecee703fbb18d7b08540
softtechSoft <65577291+softtechSoft@users.noreply.github.com>
2021-01-02 13:25:38 +0900

    Add files via upload

---
+++ src/main/java/com/ems/entity/SalaryInfo.java
@@ -0,0 +1,183 @@
+package com.ems.entity;
+
+
+public class SalaryInfo
+{
+	private String employeeName="0";
+	private String employeeID="0";
+	private String month="0";
+	private String paymentDate="0";
+	private String base="0";
+	private String overTimePlus="0";
+	private String shortageReduce="0";
+	private String transportExpense="0";
+	private String allowancePlus="0";
+	private String allowanceReduce="0";
+	private String allowanceReason="0";
+	private String welfareSelf="0";
+	private String welfareComp="0";
+	private String welfareBaby="0";
+	private String eplyInsSelf="0";
+	private String eplyInsComp="0";
+	private String eplyInsWithdraw="0";
+	private String withholdingTax="0";
+	private String municipalTax="0";
+	private String rental="0";
+	private String rentalMgmtFee="0";
+	private String sum="0";
+	private String deleteFlg="0";
+	private String insertDate="0";
+
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(String employeeID) {
+		this.employeeID = employeeID;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public String getPaymentDate() {
+		return paymentDate;
+	}
+	public void setPaymentDate(String paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+	public String getBase() {
+		return base;
+	}
+	public void setBase(String base) {
+		this.base = base;
+	}
+	public String getOverTimePlus() {
+		return overTimePlus;
+	}
+	public void setOverTimePlus(String overTimePlus) {
+		this.overTimePlus = overTimePlus;
+	}
+	public String getShortageReduce() {
+		return shortageReduce;
+	}
+	public void setShortageReduce(String shortageReduce) {
+		this.shortageReduce = shortageReduce;
+	}
+	public String getTransportExpense() {
+		return transportExpense;
+	}
+	public void setTransportExpense(String transportExpense) {
+		this.transportExpense = transportExpense;
+	}
+	public String getAllowancePlus() {
+		return allowancePlus;
+	}
+	public void setAllowancePlus(String allowancePlus) {
+		this.allowancePlus = allowancePlus;
+	}
+	public String getAllowanceReduce() {
+		return allowanceReduce;
+	}
+	public void setAllowanceReduce(String allowanceReduce) {
+		this.allowanceReduce = allowanceReduce;
+	}
+	public String getAllowanceReason() {
+		return allowanceReason;
+	}
+	public void setAllowanceReason(String allowanceReason) {
+		this.allowanceReason = allowanceReason;
+	}
+	public String getWelfareSelf() {
+		return welfareSelf;
+	}
+	public void setWelfareSelf(String welfareSelf) {
+		this.welfareSelf = welfareSelf;
+	}
+	public String getWelfareComp() {
+		return welfareComp;
+	}
+	public void setWelfareComp(String welfareComp) {
+		this.welfareComp = welfareComp;
+	}
+	public String getWelfareBaby() {
+		return welfareBaby;
+	}
+	public void setWelfareBaby(String welfareBaby) {
+		this.welfareBaby = welfareBaby;
+	}
+	public String getEplyInsSelf() {
+		return eplyInsSelf;
+	}
+	public void setEplyInsSelf(String eplyInsSelf) {
+		this.eplyInsSelf = eplyInsSelf;
+	}
+	public String getEplyInsComp() {
+		return eplyInsComp;
+	}
+	public void setEplyInsComp(String eplyInsComp) {
+		this.eplyInsComp = eplyInsComp;
+	}
+	public String getEplyInsWithdraw() {
+		return eplyInsWithdraw;
+	}
+	public void setEplyInsWithdraw(String eplyInsWithdraw) {
+		this.eplyInsWithdraw = eplyInsWithdraw;
+	}
+	public String getWithholdingTax() {
+		return withholdingTax;
+	}
+	public void setWithholdingTax(String withholdingTax) {
+		this.withholdingTax = withholdingTax;
+	}
+	public String getMunicipalTax() {
+		return municipalTax;
+	}
+	public void setMunicipalTax(String municipalTax) {
+		this.municipalTax = municipalTax;
+	}
+	public String getRental() {
+		return rental;
+	}
+	public void setRental(String rental) {
+		this.rental = rental;
+	}
+	public String getRentalMgmtFee() {
+		return rentalMgmtFee;
+	}
+	public void setRentalMgmtFee(String rentalMgmtFee) {
+		this.rentalMgmtFee = rentalMgmtFee;
+	}
+	public String getSum() {
+		return sum;
+	}
+	public void setSum(String sum) {
+		this.sum = sum;
+	}
+	public String getDeleteFlg() {
+		return deleteFlg;
+	}
+	public void setDeleteFlg(String deleteFlg) {
+		this.deleteFlg = deleteFlg;
+	}
+	public String getInsertDate() {
+		return insertDate;
+	}
+	public void setInsertDate(String insertDate) {
+		this.insertDate = insertDate;
+	}
+	public String getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+	private String updateDate="0";
+}
