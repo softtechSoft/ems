2f9022999dafd63c165fae9aed9ee0a3452d9b43
Yang Hang <Yang Hang@DESKTOP-P7A6GFA>
2021-01-06 20:11:59 +0900

    更新

--- src/main/resources/static/index/js/index.js
+++ src/main/resources/static/index/js/index.js
@@ -1,41 +1,30 @@
 var timeClean;
-//window.addEventlistener添加体格页面事件监听，DOMContentLoaded浏览器打开事件
 window.addEventListener('DOMContentLoaded',
 function()
 {
-    //声明变量的方式 用var声明 ，querySelector(样式器的名字，这里取的是.login)
     var login=document.querySelector('.login');
     var h1=document.querySelector('h1');
     var h2=document.querySelector('h2');
     var title=document.querySelector('.title');
     var footer=document.querySelector('footer');
-    //获取宽度
     if(title.clientWidth<870)
     {
-        //title左边距
         title.style.marginLeft="0px";
-        //title的背景样式
         title.style.backgroundPosition="center center";
-        //清楚h1和h2的内容
         h1.innerHTML="";
         h2.innerHTML="";
         login.style.left=(title.clientWidth-login.clientWidth)/2+"px";
     }
     else
     {
-        //login样式靠左
         login.style.left="65%";
     }
-    //footer上边距线-23大于login的底部
     if((footer.getBoundingClientRect().top-23)<login.getBoundingClientRect().bottom)
     {
-        //清除footer内容
         footer.innerHTML="";
     }
-    //添加窗口大小变化的监听
     window.addEventListener('resize', function()
     {
-        //客户端边距小于870
         if(title.clientWidth<870)
         {
             login.style.left=(title.clientWidth-login.clientWidth)/2+"px";
@@ -62,34 +51,24 @@ function()
         }
     },false);
 },false);
-//验证账户密码
 function regularVerify(actor)
 {   
-    //声明账户名字=id用户名信息，大小写字母与数字组合的模式
     var userName={id:"userMessage",pattern:/[A-Za-z0-9_]{1,15}@[A-Za-z0-9]{1,10}.com/,message:"メールをお入力ください"};
-    //声明，密码=id密码信息，任意字符组合的模式
     var password={id:"passwordMessage",pattern:/^[^ ]+$/,message:"パスワードをお入力ください"};
-    //账号状态
     var userStatus = true;
     actor.style.borderColor='#0aada8';
-    //switch变量表达
     switch(actor.id)
     {
-        //案件用户名不正确阻止，密码不正确阻止
         case "user": userStatus=verify(userName);break;
         case "password":if(userStatus)verify(password);break;
     }
-    //校验obj
     function verify(obj)
     {
-        //加入非obj模式测试
         if(!obj.pattern.test(actor.value))
         {
             actor.style.color='#ff0000';
             document.getElementById(obj.id).innerHTML=obj.message;
-            //错误
             userStatus=false;
-            //返回用户状态
             return userStatus;
         }
         return true;
