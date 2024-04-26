27391873f3c5e4028ac6fb071176dc578a34050f
weilanhai520 <151887585+weilanhai520@users.noreply.github.com>
2024-04-26 15:53:01 +0900

    11

---
+++ src/main/java/com/softtech/entity/YukyuInfo.java
@@ -0,0 +1,66 @@
+package com.softtech.entity;
+
+
+//Entity
+//Table(name = "yukyu")
+public class YukyuInfo {
+
+	//社員ID
+	private String employeeID;
+
+	//年度
+    private String nendo;
+
+    //総日数
+    private String totalDay;
+
+    //消化日数
+    private String usedDay;
+
+    //作成日
+    private String insertDate;
+
+    //更新日
+    private String updateDate;
+
+
+    // Getters and setters
+	public String getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(String employeeID) {
+		this.employeeID = employeeID;
+	}
+	public String getNendo() {
+		return nendo;
+	}
+	public void setNendo(String nendo) {
+		this.nendo = nendo;
+	}
+	public String getTotalDay() {
+		return totalDay;
+	}
+	public void setTotalDay(String totalDay) {
+		this.totalDay = totalDay;
+	}
+	public String getUsedDay() {
+		return usedDay;
+	}
+	public void setUsedDay(String usedDay) {
+		this.usedDay = usedDay;
+	}
+	public String getInsertDate() {
+		return insertDate;
+	}
+	public void setInsertDate(String insertDate) {
+		this.insertDate = insertDate;
+	}
+	public String getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+
+
+}
